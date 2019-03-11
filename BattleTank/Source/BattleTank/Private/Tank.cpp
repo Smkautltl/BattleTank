022e75e71e483fc1b9dacdc3e3cc7b720e41341b
DESKTOP-PQ8QHKV\matt <Matthewcpye@hotmail.com>
2019-03-11 21:24:51 +0000

    Added sub object AimingComponent, this is where we ill delegate the aiming of the tank to

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -2,12 +2,13 @@
 
 #include "Tank.h"
 
-
 // Sets default values
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
 
 }
 
@@ -27,9 +28,7 @@ void ATank::Tick(float DeltaTime)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	auto OurTankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *OurTankName, *HitLocation.ToString())
-
+	TankAimingComponent->AimAt(HitLocation);
 }
 
 // Called to bind functionality to input
